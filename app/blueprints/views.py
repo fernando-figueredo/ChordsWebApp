ef511297535b34d1196f262d928879dc78667999
Fernando Luiz de Figueredo <feluiz.figueredo@gmail.com>
2021-07-06 21:33:42 -0300

    Extração de áudio do YouTube

--- app/blueprints/views.py
+++ app/blueprints/views.py
@@ -1,9 +1,11 @@
+from __future__ import unicode_literals
 from flask import Flask, render_template, request, redirect, url_for, flash, send_file, redirect
 from werkzeug.utils import secure_filename
 import os
 import pac
 
 from subprocess import Popen, PIPE
+import youtube_dl
 from app.blueprints.forms import *
 from app.blueprints.functions import *
 
@@ -35,11 +37,30 @@ def init_app(app):
             pac.convert_wav_to_16bit_mono("app/static/audio.wav", "app/static/audio.wav")
             return redirect("/")
 
+    @app.route('/extract', methods=['GET', 'POST'])
+    def extract():
+        link = request.form['musicName']
+
+        ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',  
+        }],
+        }
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([link])
+
+        return redirect("/")
+
+'''
     @app.route('/acordes', methods=['GET', 'POST'])
     def acordes():
 
-        idioma = request.form['idiomaSelec']
-        nomemusica = request.form['musicName']
+        #idioma = request.form['idiomaSelec']
+        #nomemusica = request.form['musicName']
          
         #Separa Instrumental dos Vocais
         separaVocais()
@@ -62,5 +83,5 @@ def init_app(app):
         
         os.chdir('D:/GitHub/ChordsWebApp')
         return send_file('../'+nomemusica+'_cifra.txt', as_attachment=True, cache_timeout=0)
-
+'''
 
